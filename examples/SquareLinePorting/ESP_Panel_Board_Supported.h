e5c74ecdf20bc956866f8d159c197eb6f04790c3
VIEWESMART <liulong085711@outlook.com>
2025-05-07 09:54:55 +0800

    Update ESP_Panel_Board_Supported.h

--- examples/SquareLinePorting/ESP_Panel_Board_Supported.h
+++ examples/SquareLinePorting/ESP_Panel_Board_Supported.h
@@ -138,7 +138,7 @@ The following products control IM through software
 #define IM1 48
 #endif
 
-#if defined(BOARD_UEDX48480021_MD80ET) || defined(BOARD_UEDX48480021_MD80E)
+#if defined(BOARD_UEDX48480021_MD80ET) || defined(BOARD_UEDX48480021_MD80E) || defined(BOARD_UEDX48480021_MD80E_V2) 
 #define KNOB21
 #endif
 #if defined(BOARD_UEDX24240013_MD50E)
